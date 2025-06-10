d7ef823325401b230954b90d4f8111d8bb288354
Ivo-velev <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>
2025-06-10 12:31:29 +0100

    oops did a lot

--- roman_numeral.py
+++ roman_numeral.py
@@ -1,3 +1,28 @@
 def romanise(num):
-    numerals = {1 : "I", 2 : "II", 3 : "III", 4 : "IV"}
-    return numerals[num]
+    ones_roman = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    tens_roman = ["", "X", "XX", "XXX", "XL", "L", " LX", "LXX", "LXXX", "XC"]
+    hundreds_roman = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    thousands_roman = ["", "M", "MM", "MMM"]
+    
+    if num > 3999:
+        raise ValueError("number is greater than maximum of 3999")
+    elif num <= -1:
+        raise ValueError("number is lower than minimum of 0")
+
+    output = ""
+    
+    thousands = num // 1000
+    output += thousands_roman[thousands]
+    num -= thousands * 1000
+    
+    hundreds = num // 100
+    output += hundreds_roman[hundreds]
+    num -= hundreds * 100
+    
+    tens = num // 10
+    output += tens_roman[tens]
+    num -= tens * 10 
+
+    output += ones_roman[num]
+
+    return output
--- roman_numeral_test.py
+++ roman_numeral_test.py
@@ -1,7 +1,7 @@
 import unittest
 from roman_numeral import romanise
 
-class TestStringMethods(unittest.TestCase):
+class TestOutputsRoman(unittest.TestCase):
     def test_does_one(self):
         self.assertEqual(romanise(1), "I")
 
@@ -13,3 +13,31 @@ class TestStringMethods(unittest.TestCase):
 
     def test_does_four(self):
         self.assertEqual(romanise(4), "IV")
+
+    def test_does_ten(self):
+        self.assertEqual(romanise(10), "X")
+
+    def test_does_eighteen(self):
+        self.assertEqual(romanise(18), "XVIII")
+
+    def test_does_100(self):
+        self.assertEqual(romanise(100), "C")
+    
+    def test_does_298(self):
+        self.assertEqual(romanise(298), "CCXCVIII")
+    
+    def test_does_1000(self):
+        self.assertEqual(romanise(1000), "M")
+
+    def test_does_1001(self):
+        self.assertEqual(romanise(1001), "MI")
+
+    def test_does_3999(self):
+        self.assertEqual(romanise(3999), "MMMCMXCIX")
+
+class TestEdgeCases(unittest.TestCase):
+    def test_doesnt_4000(self):
+        self.assertRaises(ValueError, romanise, 4000)
+    
+    def test_doesnt_minus(self):
+        self.assertRaises(ValueError, romanise, -1)
